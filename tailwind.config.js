4673c7902faedbd6b879d1b3e20cf1863f11f4bb
xnousnow <xnou.snow@gmail.com>
2023-12-09 20:07:50 +0900

    기본 폰트로 Pretendard 사용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+// noinspection JSUnusedGlobalSymbols
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        'sans': ['Pretendard', 'sans-serif']
+      }
+    }
   },
   plugins: []
 }
