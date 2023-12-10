8ac59ba12cbf10e66581fb7dc1c6121846444a4e
xnousnow <xnou.snow@gmail.com>
2023-12-10 18:00:34 +0900

    아이템 목록 디자인

---
+++ src/components/ItemList.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let items: string[]
+</script>
+
+<aside class="m-5 rounded-2xl bg-white w-72 pt-8">
+  <h1 class="text-3xl font-bold mb-2 px-5">ItemLog</h1>
+  <ul class="flex flex-col px-2 rounded-xl overflow-hidden">
+    {#each items as item}
+      <li class="flex">
+        <a href="/item/{item.id}" class="pl-4 rounded-lg hover:bg-neutral-100 h-10 grow flex items-center w-full">
+          {item}
+        </a>
+      </li>
+    {/each}
+  </ul>
+</aside>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import ItemList from '../components/ItemList.svelte'
+
+  const items = ['item 1', 'item 2', 'item 3']
+</script>
+
+<div class="w-screen h-[100dvh] flex bg-neutral-100">
+  <ItemList {items} />
+</div>
