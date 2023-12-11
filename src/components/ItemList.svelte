0fc2f80450cf44855eeac6aab4105bc6f8cf20e3
xnousnow <xnou.snow@gmail.com>
2023-12-11 14:36:53 +0900

    설치

--- src/components/ItemList.svelte
+++ src/components/ItemList.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <aside class="m-5 rounded-2xl bg-white w-72 pt-8">
-  <h1 class="text-3xl font-bold mb-2 px-5">ItemLog</h1>
+  <div class="flex justify-between">
+    <h1 class="text-3xl font-bold mb-2 px-5">ItemLog</h1>
+  </div>
   <ul class="flex flex-col px-2 rounded-xl overflow-hidden">
     {#each items as item}
       <li class="flex">
