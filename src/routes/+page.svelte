4588334049b018a6a4fa4c4cae8b37bda7865279
xnousnow <xnou.snow@gmail.com>
2023-12-31 14:10:40 +0900

    아이템 목록을 layout으로 이동

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,1 @@
-<script lang="ts">
-  import type { Item } from '../stores'
-  import { data as dataStore } from '../stores'
-
-  import ItemList from '../components/ItemList.svelte'
-
-  let data: Item[] = []
-  dataStore.subscribe((items: Item[]) => data = items)
-</script>
-
-<div class="w-screen h-[100dvh] flex bg-neutral-100">
-  <ItemList {data} />
-</div>
+Select Item!
