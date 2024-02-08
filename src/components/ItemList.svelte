0905756276fd5212d350a4fb37d5920191f0f961
xnousnow <xnou.snow@gmail.com>
2024-02-08 12:26:36 +0900

    선택한 아이템 표시

--- src/components/ItemList.svelte
+++ src/components/ItemList.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
   import type { Item } from '../stores'
+  import { data as dataStore } from '../stores'
+
+  import { page } from '$app/stores'
 
   import Plus from '~icons/lucide/Plus'
 
   export let data: Item[]
+
+  let itemData: Item[]
+  dataStore.subscribe(e => (itemData = e))
+  $: selected = itemData.find((i: Item) => i.id == parseInt($page.params.id)) as Item
 </script>
 
 <aside class="rounded-2xl bg-white w-72 pt-8 shrink-0">
@@ -16,7 +23,9 @@
   <ul class="flex flex-col px-2 rounded-xl overflow-hidden">
     {#each data as item (item.id)}
       <li class="flex">
-        <a href="/item/{item.id}" class="pl-4 rounded-lg hover:bg-neutral-100 h-10 grow flex items-center w-full">
+        <a href="/item/{item.id}"
+           class="gap-2 rounded-lg hover:bg-neutral-100 active:bg-neutral-200 h-10 grow flex items-center w-full">
+          <div class="ml-1 h-8 w-1.5 bg-neutral-200 rounded-full" class:opacity-0={item.name !== selected.name}></div>
           {item.name}
         </a>
       </li>
