db93172cea4fff6532918afad76656be9a9c7b77
xnousnow <xnou.snow@gmail.com>
2024-02-11 16:24:33 +0900

    아이템 편집/삭제 버튼 추가

--- src/components/ItemList.svelte
+++ src/components/ItemList.svelte
@@ -5,6 +5,8 @@
   import { page } from '$app/stores'
 
   import Plus from '~icons/lucide/Plus'
+  import Trash2 from '~icons/lucide/Trash-2'
+  import Pencil from '~icons/lucide/Pencil'
 
   export let data: Item[]
 
@@ -22,11 +24,20 @@
   </div>
   <ul class="flex flex-col px-2 rounded-xl overflow-hidden">
     {#each data as item (item.id)}
-      <li class="flex">
+      <li class="flex group">
         <a href="/item/{item.id}"
-           class="gap-2 rounded-lg hover:bg-neutral-100 active:bg-neutral-200 h-10 grow flex items-center w-full">
-          <div class="ml-1 h-8 w-1.5 bg-neutral-200 rounded-full" class:opacity-0={item.name !== selected.name}></div>
+           class="gap-2 rounded-xl hover:bg-neutral-100 active:bg-neutral-200 h-10 grow flex items-center w-full pr-1.5">
+          <div class="ml-1 h-5 w-1.5 bg-neutral-200 rounded-full" class:opacity-0={item.name !== selected.name}></div>
           {item.name}
+          <div class="grow"></div>
+          <div class="hidden group-hover:flex hover:flex">
+            <button class="hover:bg-black/10 w-7 h-7 flex justify-center items-center rounded-lg">
+              <Pencil class="w-5 h-5 text-neutral-400 active:text-neutral-500" />
+            </button>
+            <button class="hover:bg-red-500/10 w-7 h-7 flex justify-center items-center rounded-lg">
+              <Trash2 class="w-5 h-5 text-red-400 active:text-red-500" />
+            </button>
+          </div>
         </a>
       </li>
     {/each}
